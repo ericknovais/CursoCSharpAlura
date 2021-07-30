5e895eb23d9382f4cc356eb4b41a241ac0fc623f
Erick Novais Da Hora - EHO <57533773+erickIteris@users.noreply.github.com>
2021-07-30 14:30:48 -0300

    Aula 5 Caracteres

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de tecnlogia " + 2021;
+            Console.WriteLine(titulo);
+
+            Console.WriteLine("Execusão finalizada. Tecle entre para sair...");
+            Console.ReadLine();
+        }
+    }
+}
